2c9066641ad35d1bd5de6cf413ba1410bc5c7caf
Richy <richy@webcomm.com.tw>
2023-08-01 20:43:24 +0800

    註解

--- iOS_HelloWKWebView/ViewController.swift
+++ iOS_HelloWKWebView/ViewController.swift
@@ -33,6 +33,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
         let strURL = "https://www.apple.com/tw/"
         let strURL2 = "http://www.bild.de/" // https 去掉s
         
+        // string -> URL -> URLRequest -> Webview.load
         if let url = URL(string: strURL2) {
             let request = URLRequest(url: url)
             myWebview.load(request)
