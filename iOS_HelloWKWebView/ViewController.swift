1a367f08bb6f1e0e66ff407470e523c07a31374a
Richy <richy@webcomm.com.tw>
2023-08-01 20:07:27 +0800

    import WebKit, connect WebView, myWebview.load

--- iOS_HelloWKWebView/ViewController.swift
+++ iOS_HelloWKWebView/ViewController.swift
@@ -6,12 +6,24 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myWebview: WKWebView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let strURL = "https://www.apple.com/tw/"
+        let strURL2 = "http://www.bild.de/" // https 去掉s
+        
+        if let url = URL(string: strURL2) {
+            let request = URLRequest(url: url)
+            myWebview.load(request)
+        }
     }
 
 
